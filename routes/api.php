c6c9bf3ae9a76620e0333ad684ba5e45e06bc1af
raymond458 <adeloraymonds7@gmail.com>
2026-07-24 14:24:57 +0300

    Task: staging actions

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/', function (Request $request) {
+    return response()->json([
+        'status' => 200,
+        'message' => 'SwiftBoda API V1',
+    ]);
+});
