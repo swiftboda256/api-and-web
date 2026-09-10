470dae49c5282ead959c5eae17d63226c37f522a
raymond458 <adeloraymonds7@gmail.com>
2026-09-10 16:07:33 +0300

    Task: Removed global search

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->login()
+            ->globalSearch(false)
             ->colors([
                 'primary' => Color::Amber,
             ])
